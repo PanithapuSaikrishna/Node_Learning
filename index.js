de3fd754454fac323597f92fe7a16bbf23e78e6b
PanithapuSaikrishna <psaikrishna.c@gmail.com>
2022-01-05 13:37:53 +0530

    bug fixes

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ const express = require('express');
 const appStartUpDebugger = require('debug')('app:startup');
 const config = require('config');
 const path = require('path');
-const logger = require('./logger');
+const logger = require('./middleware/logger');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const authenticate = require('./authenticate');
 const Joi = require('joi');
 const app = express();
-
-app.use('/', homeRouter);
-app.use('/api/courses', coursesRouter);
 
 console.log('Application name: '+ config.get('name'));
 console.log('Mail server: '+ config.get('mail.host'));
@@ -25,7 +22,7 @@ app.use(authenticate);
 
 app.use(helmet());
 if (app.get('env') === 'development') { // this is how we can tell that our app is in which env (dev,testing,prod)
-    app.use(morgan('short'));
+    app.use(morgan('common'));
     appStartUpDebugger('Morgan enabled..')
 }
 console.log(process.env.NODE_ENV);
@@ -34,7 +31,8 @@ console.log(app.get('env'));
 // app.engine('pug', require('pug').__express)
 // app.set('view engine', 'pug');
 // app.set('views', path.join(__dirname, 'views')); //default // Not Working as expected
-
+app.use('/', homeRouter);
+app.use('/api/courses', coursesRouter);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`)
